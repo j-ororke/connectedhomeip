1a1d2e198958cc39d6d5cba12b163ea5c62a9162
Restyled.io <commits@restyled.io>
2026-02-09 17:29:05 +0000

    Restyled by ruff

--- src/python_testing/TC_IDM_1_4.py
+++ src/python_testing/TC_IDM_1_4.py
@@ -49,7 +49,6 @@ import matter.testing.matchers as matchers
 from matter.exceptions import ChipStackError
 from matter.interaction_model import InteractionModelError, Status
 from matter.testing.decorators import async_test_body
-from matter.testing.matter_testing import MatterBaseTest
 from matter.testing.runner import TestStep, default_matter_test_main
 
 log = logging.getLogger(__name__)
