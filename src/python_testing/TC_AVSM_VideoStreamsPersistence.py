18a49d9c2e52ee5d34a723872f31712835d1f5e2
Restyled.io <commits@restyled.io>
2026-04-10 20:38:28 +0000

    Restyled by ruff

--- src/python_testing/TC_AVSM_VideoStreamsPersistence.py
+++ src/python_testing/TC_AVSM_VideoStreamsPersistence.py
@@ -31,7 +31,7 @@
 #       --trace-to json:${TRACE_TEST_JSON}.json
 #       --trace-to perfetto:${TRACE_TEST_PERFETTO}.perfetto
 #       --endpoint 1
-#       --no-wildcard-subscription 
+#       --no-wildcard-subscription
 #     factory-reset: true
 #     quiet: true
 # === END CI TEST ARGUMENTS ===
