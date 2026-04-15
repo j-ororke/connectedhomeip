17479f74881a854e299e1f0b81d227cf86359974
Jake Ororke <jororke@csa-iot.org>
2026-04-15 09:38:00 -0700

    Apply suggestions from code review by enkiusz
    Co-authored-by: Maciej Grela <enki@fsck.pl>

--- src/python_testing/TC_ECOINFO_2_2.py
+++ src/python_testing/TC_ECOINFO_2_2.py
@@ -168,21 +168,19 @@ class TC_ECOINFO_2_2(MatterBaseTest):
 
         self.step(1)
         self.step("1a")
-        root_node_endpoint = 0
+        ROOT_NODE_ENDPOINT = 0
         parts_list_1a = await self.read_single_attribute_check_success(
             dev_ctrl=dev_ctrl,
             node_id=dut_node_id,
             cluster=Clusters.Descriptor,
             attribute=Clusters.Descriptor.Attributes.PartsList,
-            endpoint=root_node_endpoint)
+            endpoint=ROOT_NODE_ENDPOINT)
 
         self.step("1b")
         set_of_endpoints_step_1 = set(parts_list_1a)
         list_of_aggregator_endpoints = []
         for endpoint in set_of_endpoints_step_1:
             device_type_list = await self.read_single_attribute_check_success(
-                dev_ctrl=dev_ctrl,
-                node_id=dut_node_id,
                 cluster=Clusters.Descriptor,
                 attribute=Clusters.Descriptor.Attributes.DeviceTypeList,
                 endpoint=endpoint)
@@ -209,11 +207,9 @@ class TC_ECOINFO_2_2(MatterBaseTest):
 
         self.step("2b")
         parts_list_2b = await self.read_single_attribute_check_success(
-            dev_ctrl=dev_ctrl,
-            node_id=dut_node_id,
             cluster=Clusters.Descriptor,
             attribute=Clusters.Descriptor.Attributes.PartsList,
-            endpoint=root_node_endpoint)
+            endpoint=ROOT_NODE_ENDPOINT)
         set_of_endpoints_step_2 = set(parts_list_2b)
 
         asserts.assert_true(set_of_endpoints_step_2.issuperset(set_of_endpoints_step_1), "Expected only new endpoints to be added")
@@ -256,11 +252,9 @@ class TC_ECOINFO_2_2(MatterBaseTest):
 
         self.step("3b")
         parts_list_3b = await self.read_single_attribute_check_success(
-            dev_ctrl=dev_ctrl,
-            node_id=dut_node_id,
             cluster=Clusters.Descriptor,
             attribute=Clusters.Descriptor.Attributes.PartsList,
-            endpoint=root_node_endpoint)
+            endpoint=ROOT_NODE_ENDPOINT)
         set_of_endpoints_step_3 = set(parts_list_3b)
 
         asserts.assert_equal(set_of_endpoints_step_3, set_of_endpoints_step_1,
